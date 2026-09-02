5ee5bd9a81b05ea389cc234e58388620a4138fe5
specrez <oshadha.dw@gmail.com>
2026-09-02 09:19:51 +0530

    Initial project setup

---
+++ test-data/users.ts
@@ -0,0 +1,17 @@
+
+export const users = {
+  standard: {
+    username: 'standard_user',
+    password: 'secret_sauce',
+  },
+
+  locked: {
+    username: 'locked_out_user',
+    password: 'secret_sauce',
+  },
+
+  invalid: {
+    username: 'wrong_user',
+    password: 'wrong_password',
+  },
+};
